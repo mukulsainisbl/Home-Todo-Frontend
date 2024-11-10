0907e310ce7c88e4d439655417008e512e073e2c
Mukul Saini <138042294+mukulsainisbl@users.noreply.github.com>
2024-11-10 14:16:21 +0530

    fix context

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AuthContextProvider from "./Context/authContextProvider.jsx";
 createRoot(document.getElementById("root")).render(
-  <AuthContextProvider>
     <BrowserRouter>
+  <AuthContextProvider>
       <App />
-    </BrowserRouter>
   </AuthContextProvider>
+    </BrowserRouter>
 );
