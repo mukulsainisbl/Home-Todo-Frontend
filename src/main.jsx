358e617ed1fc66d40d76f926511122e359612631
Mukul Saini <138042294+mukulsainisbl@users.noreply.github.com>
2024-11-10 14:26:53 +0530

    Fix context

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import AuthContextProvider from "./Context/authContextProvider.jsx";
+import AuthContextProvider from "./Context/AuthContextProvider.jsx"
+
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
   <AuthContextProvider>
+
+    <BrowserRouter>
       <App />
-  </AuthContextProvider>
     </BrowserRouter>
+  </AuthContextProvider>
 );
